e7fb7e26bd3f2034de8bdd1d2e2d2092059ad39e
pedroapa <111130865+pedroapa@users.noreply.github.com>
2022-08-25 13:43:00 -0300

    Ex1 resolvido
    Peguei esse ex no livro "Como aprender C" e é sobre estrutura de controle(executar comando atras do outro) e if simples, mas eu acabei fazendo uma versão que otimiza-se com if  aninhado.

---
+++ php/ex1/versao-simples/processando.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../_css/estilo.css">
+    <title>Fazendo 4 Calculos</title>
+</head>
+<body>
+    <div>
+        <?php
+            $valor1 = isset($_GET["val1"])?$_GET["val1"]:0;
+            $valor2 = isset($_GET["val2"])?$_GET["val2"]:0;
+            $valor3 = isset($_GET["val3"])?$_GET["val3"]:0;
+            $maior = 0;
+            $menor = 9999;
+            echo "Os valores inteiros inseridios foram: $valor1 $valor2 $valor3 <br>";
+            $soma = $valor1 + $valor2 + $valor3;
+            $media = ($valor1 + $valor2 + $valor3) / 3;
+            $produto  = $valor1 * $valor2 * $valor3;
+            echo "  SOMA = $soma <br>MÉDIA = $media <br>PRODUTO = $produto <br>";
+
+            # Encontrar o maior com if simples
+           if ($valor1 > $valor2 && $valor1 > $valor3) {
+                $maior = $valor1;
+            }
+
+            if ($valor2 > $valor1 && $valor2 > $valor3) {
+                $maior = $valor2;
+            }
+
+            if ($valor3 > $valor1 && $valor3 > $valor2) {
+                $maior = $valor3;
+            }
+
+            # Agora o menor
+            if ($valor1 < $valor2 && $valor1 < $valor3) {
+                $menor = $valor1;
+            }
+
+            if ($valor2 < $valor1 && $valor2 < $valor3) {
+                $menor = $valor2;
+            }
+
+            if ($valor3 < $valor1 && $valor3 < $valor2) {
+                $menor = $valor3;
+            }
+
+            echo "O maior valor: $maior <br>O menor valor: $menor <br>";
+
+        ?>
+        <a href="cal-3nums.html" target="_self" rel="prev">Voltar</a>
+    </div>
+    
+</body>
+</html>
