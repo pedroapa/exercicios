e54f97a31820e6458e44677d8cdd2f1e03111324
pedroapa <111130865+pedroapa@users.noreply.github.com>
2022-08-26 11:38:57 -0300

    Resolvi o erro
    Erro era que eu só achei o maior, sendo que era importante encontrar o menor também, porque eu não sei aonde vai estar o maior número entre as duas variáveis. Com essa duas guardando informação certo, consigo por certinho lá na divisão que vai ser o maior número dividido pelo menor.

--- php/ex4/multiplos.php
+++ php/ex4/multiplos.php
@@ -13,17 +13,20 @@
             $numero1 = isset($_GET["n1"])? $_GET["n1"]: 0;
             $numero2 = isset($_GET["n2"])? $_GET["n2"]: 0;
             $maior = 0;
+            $menor = 9999;
             echo "Números escolhidos: $numero1 e $numero2 <br>";
 
             # Primeiro encontrar o maior valor porque envolve uma divisão
             if ($numero1 > $numero2) {
                 $maior = $numero1;  
+                $menor = $numero2;
             }
             else {
                 $maior = $numero2;
+                $menor = $numero1;
             }
 
-            echo ( ($maior % $numero2 == 0 )? "Os dois números $numero2 e $numero1 são multiplos. <br>": "Os dois números $numero2 e $numero1 não são multiplos. <br>");
+            echo ( ($maior % $menor == 0 )? "Os dois números $numero2 e $numero1 são multiplos. <br>": "Os dois números $numero2 e $numero1 não são multiplos. <br>");
         ?>
         <a href="num-multiplos.html" target="_self" rel="prev">Voltar</a>
     </div>
