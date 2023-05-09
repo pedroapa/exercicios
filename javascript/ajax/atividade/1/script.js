befac754c7773c9b66419310644c705c09825002
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-05-09 10:43:03 -0300

    Atualizando

--- javascript/ajax/atividade/1/script.js
+++ javascript/ajax/atividade/1/script.js
@@ -1,13 +1,12 @@
 //Criação dos objetos   
 let res = window.document.querySelector('div#result')
-let lista = window.document.querySelector('select#cSelProd')
+//let lista = window.document.querySelector('select#cSelProd')
 
 function pesquisar(){
     //Parte responsavel por AJAX
     const xhttp = new XMLHttpRequest();
 
     xhttp.onload = function(){
-        //res.innerHTML = this.responseText
         res.innerHTML = this.responseText;
     }
     xhttp.open("GET", "pegaQtd.php");
