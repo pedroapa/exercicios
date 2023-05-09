df5600f2fc5d22d63f84cdc7cdcb0cea80844fca
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-05-09 13:30:27 -0300

    Exercicio pronto
    Conseguir praticar o AJAX com sucesso, consegui selecionar um produto e descobrir a sua quantidade no banco, fazendo isso dinamicamente sem precisar recarregar a pagina.

--- javascript/ajax/atividade/1/pegaQtd.php
+++ javascript/ajax/atividade/1/pegaQtd.php
@@ -2,7 +2,14 @@
 include "conexao.php";
 
 $vIdProd = isset($_GET["idProd"])? $_GET["idProd"]: 0;
-$resposta = "Escolheu o produto $vIdProd";
-echo $resposta;
+
+//Mysql
+$sql = "SELECT amount FROM products
+WHERE id = $vIdProd";
+$res = mysqli_query($conn, $sql);
+$linha = mysqli_fetch_array($res);
+$vQtd = $linha[0];
+
+echo $vQtd;
 
 ?>
