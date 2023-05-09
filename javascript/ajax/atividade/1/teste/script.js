a545b8f2a395bec9a8d8037db8e7435f1004004b
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-05-09 10:35:03 -0300

    Preparando para o teste
    Vou testar outra coisa agora. Vou criar um objeto e através dele mexer no seu conteúdo.  Ex: 'res.innerHTML = this.responseText'

--- javascript/ajax/atividade/1/teste/script.js
+++ javascript/ajax/atividade/1/teste/script.js
@@ -1,10 +1,10 @@
 function processa() {
     /*var t1 = document.getElementById("termo1").value;
     var t2 = document.getElementById("termo2").value;*/
-    const xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest()
     xhttp.onload = function() {
-        document.getElementById("result").innerHTML = this.responseText;
+        document.getElementById("result").innerHTML = this.responseText
     }
-    xhttp.open("GET", "pegaQtd.php");
-    xhttp.send();    
+    xhttp.open("GET", "pegaQtd.php")
+    xhttp.send() 
 }      
