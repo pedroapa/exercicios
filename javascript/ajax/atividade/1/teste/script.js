8014672cf20d91dd3765c552911ebf3bb01753de
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-05-09 10:46:41 -0300

    Atualizando arquivo que funcionou
    Estou atualizando o arquivo que esta funcionando. Não precisa daquele ponto e virgula e nem o espaço.

--- javascript/ajax/atividade/1/teste/script.js
+++ javascript/ajax/atividade/1/teste/script.js
@@ -2,8 +2,8 @@ let res = window.document.querySelector('div#result')
 
 function processa() {
     const xhttp = new XMLHttpRequest()
-    xhttp.onload = function() {
-        res.innerHTML = this.responseText;
+    xhttp.onload = function(){
+        res.innerHTML = this.responseText
     }
     xhttp.open("GET", "pegaQtd.php")
     xhttp.send() 
