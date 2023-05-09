6a1b4e74d49fe2e716b7297bbaffac947b809ade
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-05-09 10:41:27 -0300

    Atualizando
    Quero dizer os dois códigos muitos parecidos para procurar se tem alguma coisa escrita diferente.

--- javascript/ajax/atividade/1/teste/script.js
+++ javascript/ajax/atividade/1/teste/script.js
@@ -1,8 +1,6 @@
 let res = window.document.querySelector('div#result')
 
 function processa() {
-    /*var t1 = document.getElementById("termo1").value;
-    var t2 = document.getElementById("termo2").value;*/
     const xhttp = new XMLHttpRequest()
     xhttp.onload = function() {
         res.innerHTML = this.responseText;
