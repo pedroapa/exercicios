6c8889d8562c5a6d344e7738bd657f1a95c7ffe6
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-05-22 10:46:56 -0300

    Pequena correção

--- javascript/ajax/JQUERY/5/jquery/codigo-mais-basico/2/script.js
+++ javascript/ajax/JQUERY/5/jquery/codigo-mais-basico/2/script.js
@@ -2,7 +2,7 @@ function testeAJAX(){
     var n1 = $("#num1").val();
     var n2 = $("#num2").val();
 
-    //soma.php?val1="+n1+"&val2="+n2
+    //"soma.php?val1="+n1+"&val2="+n2
     //`soma.php?val1=${n1}&val2=${n2}`
     //"soma.php?val1=11&val2=8"
 
