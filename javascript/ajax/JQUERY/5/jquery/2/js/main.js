f0afa9a4e3426e74bf0d9a31dcc96d7b4b738bdf
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-05-22 11:40:48 -0300

    Voltei pro  codigo simples
    Vou tentar resolver esse negocio e CORS. Qualquer coisa eu peço ajuda pro meus colegas.

--- javascript/ajax/JQUERY/5/jquery/2/js/main.js
+++ javascript/ajax/JQUERY/5/jquery/2/js/main.js
@@ -5,23 +5,8 @@ function buscaCep(){
     let searc = cep.replace("-", "")
     console.log(searc)
 
-    $.ajax({
-        method: "GET",
-        url: "https://viacep.com.br/"+searc+"/json", 
-    })
-    .done( function(resposta){
-        console.log('Deu certo, voltei com resposta do servidor')
-    })
-    .fail( function (jqXHR, textStatus, resposta){
-        console.log('Aconteceu um erro: '+resposta)
-    })
-
-    /*
     //Usar o ajax com jquery
     $.get("https://viacep.com.br/"+searc+"/json", function(resposta){
         console.log('Deu certo, voltei com resposta do servidor')
     })
-    */
-
-    
 }
