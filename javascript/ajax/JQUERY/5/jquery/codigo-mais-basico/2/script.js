5614835c76b05672633cf175a74b97074ddd3489
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-05-22 10:37:00 -0300

    Problema de funcionar
    O código não esta funcionando, e eu já tentei de tudo, simplesmente não funciona. Talvez seja algo que eu não esteja vendo... Vou pedir ajuda.

--- javascript/ajax/JQUERY/5/jquery/codigo-mais-basico/2/script.js
+++ javascript/ajax/JQUERY/5/jquery/codigo-mais-basico/2/script.js
@@ -2,22 +2,12 @@ function testeAJAX(){
     var n1 = $("#num1").val();
     var n2 = $("#num2").val();
 
+    //soma.php?val1="+n1+"&val2="+n2
+    //
+    //"soma.php?val1=11&val2=8"
 
-    $.get("soma.php?val1="+n1+"&val2="+n2, function(resultado){
-        $("#resut").html(resultado)
+    $.get(`soma.php?val1=${n1}&val2=${n2}`, function(resultado){
+        $("#resut").html(resultado);
     })
-
-    /*
-    $.ajax({
-        method: "GET",
-        url: "soma.php",
-        data: { val1: n1, val2: n2 }
-    })
-    .done( function(msg){
-        $("#result").html(msg);
-    })
-    .fail(function (jqXHR, textStatus, msg){
-        alert(msg);
-    });   
-    */
+    
 }
