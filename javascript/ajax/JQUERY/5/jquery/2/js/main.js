46da8e89135eba03b3e2aa43952d3f27a64eb916
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-05-22 11:24:32 -0300

    Problema
    Fiz o codigo certo, estou mandando por get o cep no url, mas esta apresentando erro no CORS. O que vi quando pesquisei um pouco de fetc, você tem que 'dizer' lincensa quando vai usar o servidor. Avisar algumas coisas antes de usar.
    Estou achando que este comando esta muito simples e não tem isso.
    Vou tentar implementar o código mais completo usando $.ajax.

--- javascript/ajax/JQUERY/5/jquery/2/js/main.js
+++ javascript/ajax/JQUERY/5/jquery/2/js/main.js
@@ -1,6 +1,14 @@
 function buscaCep(){
     //Criando os controles
     let cep = $("#cep").val();
+    //retirando o hifen
+    let searc = cep.replace("-", "")
+    console.log(searc)
 
-    console.log(cep)
+    //Usar o ajax com jquery
+    $.get("https://viacep.com.br/"+searc+"/json", function(resposta){
+        console.log('Deu certo, voltei com resposta do servidor')
+    })
+
+    
 }
