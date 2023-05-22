fbd39dcc953ecd0bf490d70b802e3f95782249b4
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-05-22 10:46:10 -0300

    Testando
    Eu tinha feito desta forma para por parâmetro pela url, mas como não estava funcionando o código, não sabia se tinha criado certo. Agora conseguir comprovar que estava certo e esta rodando.

--- javascript/ajax/JQUERY/5/jquery/codigo-mais-basico/2/script.js
+++ javascript/ajax/JQUERY/5/jquery/codigo-mais-basico/2/script.js
@@ -3,11 +3,11 @@ function testeAJAX(){
     var n2 = $("#num2").val();
 
     //soma.php?val1="+n1+"&val2="+n2
-    //
+    //`soma.php?val1=${n1}&val2=${n2}`
     //"soma.php?val1=11&val2=8"
 
-    $.get(`soma.php?val1=${n1}&val2=${n2}`, function(resultado){
-        console.log(resultado)
+    $.get("soma.php?val1="+n1+"&val2="+n2, function(resultado){
+        //console.log(resultado)
         $("#result").html(resultado);
     })
     
