66f80e73ed9ced208d688d760b9290f3e9b5e864
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-05-22 11:33:32 -0300

    Tentativa
    Estou tentando usar o $.ajax para ver se o problema é o CORS mesmo, se com um comando mais completo se vai rodar. Porem, já encontrei um dificuldade, a api precisa que mandando o cep no meio da url, mas estou usando o jeito que separa o o dado da url para o atributo 'data'. Vou ver se assim o código funciona.

--- javascript/ajax/JQUERY/5/jquery/2/js/main.js
+++ javascript/ajax/JQUERY/5/jquery/2/js/main.js
@@ -5,10 +5,24 @@ function buscaCep(){
     let searc = cep.replace("-", "")
     console.log(searc)
 
+    $.ajax({
+        method: "GET",
+        url: "https://viacep.com.br/json",
+        data: seacr
+    })
+    .done( function(resposta){
+        console.log('Deu certo, voltei com resposta do servidor')
+    })
+    .fail( function (jqXHR, textStatus, resposta){
+        console.log('Aconteceu um erro: '+resposta)
+    })
+
+    /*
     //Usar o ajax com jquery
     $.get("https://viacep.com.br/"+searc+"/json", function(resposta){
         console.log('Deu certo, voltei com resposta do servidor')
     })
+    */
 
     
 }
