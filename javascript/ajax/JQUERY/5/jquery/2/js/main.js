70072870a8a885be8f2f8941f9822cf59a4e7b5b
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-05-24 09:19:21 -0300

    Arrumei id de alguns inputs
    Arrumei porque pra ficar melhor de entender e estava daquela forma porque tinha uma forma que resolve com fetch e seria melhor se o input tivesse o mesmo nome das propriedade do objeto. Ex: no input da cidade coloquei o id 'localidade' por é este nome que api usa para a informação de cidade.
    Agora esta de um jeito mais simples, o campo cidade tem o id cidade.

--- javascript/ajax/JQUERY/5/jquery/2/js/main.js
+++ javascript/ajax/JQUERY/5/jquery/2/js/main.js
@@ -3,16 +3,13 @@ function buscaCep(){
     let cep = $("#cep").val();
     //retirando o hifen
     let searc = cep.replace("-", "")
-    console.log(searc)
 
     //Usar o ajax com jquery
     $.get("https://viacep.com.br/ws/"+searc+"/json/", function(resposta){
             $("#logradouro").val(resposta.logradouro)
             $("#bairro").val(resposta.bairro)
-            //Cidade
-            $("#localidade").val(resposta.localidade)
-            //Estado
-            $("#uf").val(resposta.uf)
+            $("#cidade").val(resposta.localidade)
+            $("#estado").val(resposta.uf)
     })
     .fail(function(jqXHR, textStatus, resposta){
         //alert('[EROOR] Houve um erro: ' + resposta)
